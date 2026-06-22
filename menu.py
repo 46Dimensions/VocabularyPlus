9f6a185a645f8d1809b9fd4e4ff69de47dfa636c
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2026-06-22 18:47:36 +0100

    Reorder cursor visibility and sleep in main execution flow

--- menu.py
+++ menu.py
@@ -92,5 +92,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print(SHOW_CURSOR, end='', flush=True)
     finally:
-        time.sleep(10)
         print(SHOW_CURSOR, end='', flush=True)
+
+    time.sleep(10)
