3e783bfda5f7022c8eddef3d18ae9052798d74b5
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2026-06-30 19:33:03 +0100

    Fix logo rendering in PowerShell

--- install.ps1
+++ install.ps1
@@ -25,7 +25,7 @@ function Write-BasicLogo {
 
 function Write-ComplexLogo {
     Write-Host "[38;5;99m🭖█🭀  🭋█🭡   [38;5;171m██████◣"
-    Write-Host"[38;5;105m🭦█🭐  🭅█🭛   [38;5;177m██   🭨█🭬"
+    Write-Host "[38;5;105m🭦█🭐  🭅█🭛   [38;5;177m██   🭨█🭬"
     Write-Host "[38;5;141m 🭖█🭀🭋█🭡    [38;5;183m██████◤"
     Write-Host "[38;5;177m 🭦█🭐🭅█🭛    [38;5;209m██"
     Write-Host "[38;5;209m  🭖██🭡     [38;5;220m██"
